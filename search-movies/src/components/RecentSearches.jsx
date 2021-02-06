19394163cdaec5bbbc53fa3d1a6602144294ead4
Sardana, Jitin <jitin.sardana@accenture.com>
2021-02-06 16:45:10 +0530

    Run Map reverse order to show the latest search on top

--- search-movies/src/components/RecentSearches.jsx
+++ search-movies/src/components/RecentSearches.jsx
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 
-function RecentSearches({lastSearchItems}) {
+function RecentSearches({ lastSearchItems }) {
     const [showRecentSearches, setShowRecentSearches] = useState(false)
     return (<>
         <div className=''>
             <button type="button" className={`btn btn-link`} onClick={() => setShowRecentSearches(!showRecentSearches)} >Toggle to See latest Searches</button><br />
             {
                 showRecentSearches && <div className='row mt-4 ml-4 justify-content-left'>
-                   {lastSearchItems.length > 0 ?
-                    <div>
-                        {
-                            lastSearchItems.map((item)=><p>{item}</p>)
-                        }
-                    </div> :
-                     <span></span>}
+                    {lastSearchItems.length > 0 ?
+                        <div className='pt-1' style={{ lineHeight: '0.25' }}>
+                            {
+                                lastSearchItems.slice(0).reverse().map((item, i) => <p>{i + 1}) {item}</p>)
+                            }
+                        </div> :
+                        <span></span>}
                 </div>
             }
 
